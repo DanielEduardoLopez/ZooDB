18ab00ea1990764691947555bf4df51374831136
Daniel Eduardo López <delopmar@gmail.com>
2023-07-29 06:57:15 -0600

    Added Species_Habitats and Habitats_Continents tables

--- DB_Implementation.sql
+++ DB_Implementation.sql
@@ -33,4 +33,32 @@ Vegetation VARCHAR(45),
 PRIMARY KEY (idHabitat)
 );
 
+-- Table Continents
+DROP TABLE IF EXISTS Continents;
+CREATE TABLE Continents(
+idContinent INT NOT NULL AUTO_INCREMENT,
+Name VARCHAR(45),
+PRIMARY KEY (idContinents)
+);
 
+-- Table Species_Habitats
+DROP TABLE IF EXISTS Species_Habitats;
+CREATE TABLE Species_Habitats(
+idSpecie INT NOT NULL,
+idHabitat INT NOT NULL,
+PRIMARY KEY (idSpecie, idHabitat),
+FOREIGN KEY (idSpecie) REFERENCES Species(idSpecie),
+FOREIGN KEY (idHabitat) REFERENCES Habitats(idHabitat)
+ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+-- Table Habitats_Continents
+DROP TABLE IF EXISTS Habitats_Continents;
+CREATE TABLE Habitats_Continents(
+idHabitat INT NOT NULL,
+idContinent INT NOT NULL,
+PRIMARY KEY (idHabitat, idContinent),
+FOREIGN KEY (idHabitat) REFERENCES Habitats(idHabitat),
+FOREIGN KEY (idContinent) REFERENCES Continents(idContinent)
+ON UPDATE CASCADE ON DELETE CASCADE
+);
