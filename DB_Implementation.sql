c9c74cf67a671f0da3c39653c53041cff07f798e
Daniel Eduardo López <delopmar@gmail.com>
2023-10-02 10:28:30 -0600

    Added view to show species by zone in the zoo

--- DB_Implementation.sql
+++ DB_Implementation.sql
@@ -960,3 +960,18 @@ BEGIN
    RETURN aguinaldo;
 
 END//
+
+
+-- VIEWS
+-- 1. Species by zone in the zoo
+DROP VIEW IF EXISTS Species_by_zone;
+CREATE VIEW Species_by_zone AS
+SELECT z.zone_name, COUNT(s.species_id) AS "Number of Species"
+FROM zone z
+INNER JOIN cage c
+ON z.zone_id = c.zone_id
+INNER JOIN species s
+ON  c.species_id = s.species_id
+GROUP BY z.zone_name;
+
+SELECT * FROM Species_by_zone;
